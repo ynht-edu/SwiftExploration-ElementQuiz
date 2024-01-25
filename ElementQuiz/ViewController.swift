e6b740325aea93af6bb3366100541ba5bfae6808
Yayat Nurhidayat <yayatnhtedu@gmail.com>
2024-01-25 13:47:38 +0700

    add quiz mode and textfield

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
+enum Mode {
+    case flashCard
+    case quiz
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var elementImage: UIImageView!
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElementIndex = 0
+    var mode: Mode = .flashCard
+    
+    @IBOutlet weak var modeSelector: UISegmentedControl!
+    
+    @IBOutlet var textField: UIView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateElement()
         // Do any additional setup after loading the view.
+        
     }
-
+    @IBAction func showAnswer(_ sender: UIButton) {
+        answerLabel.text = elementList[currentElementIndex]
+    }
+    @IBAction func next(_ sender: UIButton) {
+        currentElementIndex = (currentElementIndex+1) % elementList.count
+        updateElement()
+    }
+    
+    func updateElement() {
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        elementImage.image = image
+        
+        answerLabel.text = "?"
+    }
 
 }
 
