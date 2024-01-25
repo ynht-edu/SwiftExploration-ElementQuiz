777b51fdc4a5f9986e4458cd6b9cfa710540fb09
Yayat Nurhidayat <yayatnhtedu@gmail.com>
2024-01-25 08:15:00 +0700

    Initial Commit

---
+++ ElementQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ElementQuiz
+//
+//  Created by Yayat Nurhidayat on 25/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
